38b9c1b82d5d46dd512e8e77b6c1f5bc4cf60db9
yasser <spookyframework@gmail.com>
2020-01-11 21:12:12 +0100

    feat: add menu config file

---
+++ config/menu.js
@@ -0,0 +1,53 @@
+/*
+ * This file is part of the Vespa package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export default [
+  {
+    "label" : "Dashboard",
+    "icon"  : "fa fa-dashboard",
+    "href"  : "/dashboard",
+    "translation"  : "TopNav.item->0"
+  },
+  {
+    "label" : "Users",
+    "icon"  : "fa fa-users",
+    "href"  : "/users",
+    "translation"  : "TopNav.item->1"
+  },
+  {
+    "label" : "Posts",
+    "icon"  : "fa fa-pencil-square",
+    "href"  : "/posts",
+    "translation"  : "TopNav.item->2"
+  },
+  {
+    "label" : "Comments",
+    "icon"  : "fa fa-commenting",
+    "href"  : "/comments",
+    "translation"  : "TopNav.item->3",
+  },
+  {
+    "label" : "Shopping",
+    "icon"  : "fa fa-shopping-bag",
+    "href"  : "/shopping",
+    "translation"  : "TopNav.item->4"
+  },
+  {
+    "label" : "Payment",
+    "icon"  : "fa fa-credit-card-alt",
+    "href"  : "/payment",
+    "translation"  : "TopNav.item->5",
+  },
+  {
+    "label" : "Settings",
+    "icon"  : "fa fa-cogs",
+    "href"  : "/settings",
+    "translation"  : "TopNav.item->6",
+  }
+];
