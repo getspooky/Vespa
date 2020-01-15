6cee93d6502d1dd976e6201e3ca14187d233b50e
yasser <spookyframework@gmail.com>
2020-01-15 23:25:25 +0100

    feat: add shared auth reducer

---
+++ app/shared/auth.reducer.js
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the Vespa package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import {AUTH_REGISTER} from 'app/containers/RegisterPage/constants';
+
+const initState = {
+  Auth: {
+    isAuthenticated: false,
+    // Get token from localStorage.
+    token: localStorage.getItem('_token'),
+  },
+};
+
+/**
+ * @export
+ * @function
+ * @name AUTH_REDUCER
+ * @param state
+ * @param type
+ * @param payload
+ * @returns {object}
+ */
+export function AUTH_REDUCER(state = initState, {type, payload}) {
+  switch (type) {
+    case AUTH_REGISTER:
+      state = {
+        Auth: {isAuthenticated: true, token: payload.response.data},
+      };
+      break;
+  }
+  return state;
+}
