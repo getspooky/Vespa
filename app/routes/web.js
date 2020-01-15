40d22f878a6a260559ad8a23e2f14cb3e3fbfa0a
yasser <spookyframework@gmail.com>
2020-01-15 23:24:49 +0100

    feat: add web.js file to handle incomming routes

---
+++ app/routes/web.js
@@ -0,0 +1,42 @@
+/*
+ * This file is part of the Vespa package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import WelcomePage from '../containers/WelcomePage'
+import LoginPage from '../containers/LoginPage';
+import RegisterPage from '../containers/RegisterPage';
+import ForgotPasswordPage from '../containers/ForgotPasswordPage';
+import DashboardPage from '../containers/DashboardPage';
+import UsersPage from '../containers/UsersPage';
+import PostsPage from '../containers/PostsPage';
+import SettingsPage from '../containers/SettingsPage';
+import ProfilePage from '../containers/ProfilePage';
+import NotFountPage from '../containers/NotFoundPage';
+
+export default [
+  // Index Page.
+  { id:'#root', path: '/', component: WelcomePage },
+  // Login Page
+  { id:'#login', path: '/login', component: LoginPage },
+  // Register Page
+  { id:'#register', path: '/register', component: RegisterPage },
+  // Forgot Password
+  { id:'#forgot', path: '/forgot/password', component: ForgotPasswordPage},
+  // Dashboard Page
+  { id:'#dashboard', path:'/dashboard', component: DashboardPage },
+  // Users
+  { id:'#users', path:'/users', component: UsersPage },
+  // Posts
+  { id:'#posts', path:'/posts', component: PostsPage },
+  // Settings
+  { id:'#settings', path:'/settings', component: SettingsPage },
+  // Profile
+  { id:'#profile', path:'/profile/:id', component: ProfilePage },
+  // Page not found
+  { id:'#pagenotfound', path:'*', component: NotFountPage}
+]
