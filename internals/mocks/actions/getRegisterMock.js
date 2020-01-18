4918c746050b22af61263c5cbae84fa4861aac9c
yasser <spookyframework@gmail.com>
2020-01-18 18:31:57 +0100

    feat: add mock folder

---
+++ internals/mocks/actions/getRegisterMock.js
@@ -0,0 +1,17 @@
+/*
+ * This file is part of the Vespa package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import casual from 'casual';
+
+export default {
+  username: `mock_api_${casual.username}`,
+  email: `mock_api_${casual.email}`,
+  password: `mock_api_${casual.password}`
+};
+
