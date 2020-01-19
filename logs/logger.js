564e5d4227ad74bbc9ab199e45f5dfd14ce8222a
yasser <spookyframework@gmail.com>
2020-01-19 17:40:41 +0100

    feat: add logger.js

---
+++ logs/logger.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+
+const chalk = require('chalk');
+const ip = require('ip');
+
+const divider = chalk.gray('\n-----------------------------------');
+
+const logger = {
+  // Called whenever there's an error on the server we want to print
+  error: err => {
+    console.error(chalk.red(err));
+  },
+
+  // Called when api-docs app starts on given port w/o errors
+  appStarted: (port, host) => {
+    console.log(`Server started ! ${chalk.green('✓')}`);
+    console.log(`
+     ${chalk.bold('Access URLs:')}${divider}
+     Localhost: ${chalk.magenta(`http://${host}:${port}`)}
+     LAN: ${chalk.magenta(`http://${ip.address()}:${port}`)}
+     ${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
+    `);
+  }
+
+};
+
+module.exports = logger;
