ea0df8d0a69ea1a2e8efc5c5842770ac407787a2
yasser <spookyframework@gmail.com>
2020-01-19 22:07:05 +0100

    update app.js config file

--- config/app.js
+++ config/app.js
@@ -7,3 +7,41 @@
  * file that was distributed with this source code.
  */
 
+import {env} from 'helpers/common';
+
+module.exports = {
+  /*
+   |--------------------------------------------------------------------------
+   | Application Name
+   |--------------------------------------------------------------------------
+   |
+   | This value is the name of your application. This value is used when
+   | Vespa needs to place the application's name in the browser tab.
+   |
+   */
+    name : env('APP_NAME'),
+
+  /*
+    |--------------------------------------------------------------------------
+    | Application Environment
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the "environment" your application is currently
+    | running in. This may determine how you prefer to configure various
+    | services the application utilizes. Set this in your ".env" file.
+    |
+    */
+    env : env('APP_ENV', 'development'),
+
+  /*
+   |--------------------------------------------------------------------------
+   | Application URL
+   |--------------------------------------------------------------------------
+   |
+   | This value is the host of your application. This value is used when the
+   | Vespa needs to start swagger api.
+   |
+   */
+   url: env('APP_HOST', 'http://localhost'),
+
+};
