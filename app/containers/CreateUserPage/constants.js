229b0eaf0c750ba1d0a05a6f7d8572f611507eb0
yasser <spookyframework@gmail.com>
2020-01-25 11:00:55 +0100

    feat: add create user page

---
+++ app/containers/CreateUserPage/constants.js
@@ -0,0 +1,15 @@
+/*
+ * This file is part of the Vespa package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @export
+ * @type {string}
+ */
+export const GET_USERS = "GET_USERS";
+export const DELETE_USERS = "DELETE_USERS";
