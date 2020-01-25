ff1206f9a5d7ff20d1bfadaaafd00c7e1903409b
yasser <spookyframework@gmail.com>
2020-01-25 22:56:18 +0100

    feat: add banner ui

---
+++ console/ui/banner.js
@@ -0,0 +1,20 @@
+/*
+ * This file is part of the Vespa package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+module.exports = `
+__  __                                     
+/\ \/\ \                                    
+\ \ \ \ \     __    ____  _____      __     
+ \ \ \ \ \  /'__`\ /',__\/\ '__`\  /'__`\   
+  \ \ \_/ \/\  __//\__, `\ \ \L\ \/\ \L\.\_ 
+   \ `\___/\ \____\/\____/\ \ ,__/\ \__/.\_\
+    `\/__/  \/____/\/___/  \ \ \/  \/__/\/_/
+                            \ \_\           
+                             \/_/             
+ ` ;
