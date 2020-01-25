7e6e7d3809ee3fd47b1f5cdc1f2b434ff2ca6d56
yasser <spookyframework@gmail.com>
2020-01-25 13:53:41 +0100

    remove service from config folder

--- config/services.js
+++
@@ -1,8 +0,0 @@
-/*
- * This file is part of the Vespa package.
- *
- * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
