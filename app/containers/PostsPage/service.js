3b1fab833ad8f9e7edf4971ba9523a198e6157b9
yasser <spookyframework@gmail.com>
2020-01-25 12:09:13 +0100

    update post api binding

--- app/containers/PostsPage/service.js
+++ app/containers/PostsPage/service.js
@@ -18,12 +18,31 @@ import binding from 'bindings/api.binding.json';
  * @returns {Promise<Response>}
  */
 export async function HTTP_REQUEST_POSTS() {
-  const requestURL = binding['[GET]_POSTS_API_BINDING'];
+  const requestURL = binding['[GET_POST]_POSTS_API_BINDING'];
   return await request(requestURL,{
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
     }
+  });
+}
+
+/**
+ * @export
+ * @async
+ * @function
+ * @name HTTP_CREATE_POSTS
+ * @param {object} payload
+ * @returns {Promise<Response>}
+ */
+export async function HTTP_CREATE_POSTS(payload) {
+  const requestURL = binding['[GET_POST]_USERS_API_BINDING'];
+  return await request(requestURL,{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload) // body data type must match "Content-Type" header
   });
 }
 
