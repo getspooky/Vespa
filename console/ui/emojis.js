5a797e49a8b178d24e06608ef6e434219f5c9754
yasser <spookyframework@gmail.com>
2020-01-26 18:12:11 +0100

    feat: add emoji ui folder

---
+++ console/ui/emojis.js
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the Vespa package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+const emoji = require('node-emoji');
+
+ module.exports = {
+   HEART: get('heart'),
+   COFFEE: get('coffee'),
+   BEER: get('beer'),
+   BROKEN_HEART: get('broken_heart'),
+   CRYING: get('crying_cat_face'),
+   HEART_EYES: get('heart_eyes_cat'),
+   JOY: get('joy_cat'),
+   KISSING: get('kissing_cat'),
+   SCREAM: get('scream_cat'),
+ };
