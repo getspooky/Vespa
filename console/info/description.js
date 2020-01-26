133c4a8b763b8c5d718aa938e667cdd4fa7d26d0
yasser <spookyframework@gmail.com>
2020-01-26 18:26:22 +0100

    add open hand emoji to the description

--- console/info/description.js
+++ console/info/description.js
@@ -7,4 +7,6 @@
  * file that was distributed with this source code.
  */
 
- module.exports = '(c) Yasser Ameur El Idrissi <getspookydev@gmail.com> 👊';
+const {OPEN_HAND} = require('../ui/emojis');
+
+module.exports = `(c) Yasser Ameur El Idrissi <getspookydev@gmail.com> ${OPEN_HAND}`;
