a85981de9fc5bfd631f7a8cc78576a4c6e9f2f79
yasser <spookyframework@gmail.com>
2020-01-26 18:32:49 +0100

    feat: add question folder

---
+++ console/question/mode.js
@@ -0,0 +1,32 @@
+
+/*
+ * This file is part of the Vespa package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+ module.exports = [
+    {
+      type: 'checkbox',
+      message: 'Select install mode',
+      name: 'toppings',
+      choices: [
+        {
+          name: 'development'
+        },
+        {
+          name: 'production'
+        }
+      ],
+      validate: function(answer) {
+        if (answer.length < 1) {
+          return 'You must choose one mode.';
+        }
+  
+        return true;
+      }
+    }
+];
